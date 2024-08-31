fa5c8082ee6e25975df7db8bb5bd5ec44d93ddbb
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-31 21:45:45 +0300

    .

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -17,5 +17,15 @@ def hash_password(password: str) -> bytes:
 
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def is_valid():
-    pass
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """_summary_
+
+    Args:
+        hashed_password (bytes): _description_
+        password (str): _description_
+
+    Returns:
+        bool: _description_
+    """
+    return bcrypt.checkpw(hashed_password, password.encode('utf-8'))
