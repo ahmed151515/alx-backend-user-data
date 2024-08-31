575ca10a67a02f4b3244fc7fdd1fbd908d72280d
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-31 18:32:45 +0300

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ from typing import List
 import re
 import logging
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS = ("name", "email", "password", "ssn", "phone")
 
 
 def get_logger() -> logging.Logger:
