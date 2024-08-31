565baa4e4580879dfff51e4d71caa4aa624ddb3b
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-08-31 18:12:31 +0300

    .

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,26 @@
 from typing import List
 
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: "
+        msg = filter_datum(self.fields, self.REDACTION,
+                           record.getMessage(), self.SEPARATOR)
+        log += msg
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
