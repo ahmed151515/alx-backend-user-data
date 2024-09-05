4b2eb6393d9bbc28cc5efc026c9b792ad2a2dbc5
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-09-05 17:39:50 +0300

    .

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,6 +19,7 @@ class SessionAuth(Auth):
         return sessionID
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """get user_ID by session_ID"""
         if session_id is None or type(session_id) != str:
             return None
         return self.user_id_by_session_id.get(session_id)
