18a5d5f89ef5904399d1ecec839908aa4c5db256
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-09-05 15:35:03 +0300

    .

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module defines the SessionAuth class, which inherits from the Auth class.
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """class i dont know yet"""
+    pass
