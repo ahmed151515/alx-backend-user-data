5e0268efd5e0b4b9f096c3c27d3a5242277c32f1
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-09-05 11:35:38 +0300

    .

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -48,7 +48,7 @@ def before_requests():
     ex_path = ['/api/v1/status/',
                '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth.require_auth(request.path, ex_path):
-        pass
+        return
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
