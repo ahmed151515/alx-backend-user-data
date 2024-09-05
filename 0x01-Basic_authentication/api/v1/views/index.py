0707c5989e84ef8bf38c53b936e6025838217979
ahmed arafa <ahmedarafa.200444@gmail.com>
2024-09-05 10:35:36 +0300

    .

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -22,6 +22,14 @@ def unauthorized() -> str:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+        test user authenticate but not allowed 
+    """
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
